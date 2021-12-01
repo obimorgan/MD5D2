3d05ccd2362f408b53f94d6175b8dd91e9c50b58
Roby Morgan <roby.c.morgan@gmail.com>
2021-12-01 13:39:11 +0100

    messing around. created a mess.

--- src/authors/index.js
+++ src/authors/index.js
@@ -3,117 +3,68 @@
 import express from "express";
 import fs from "fs";
 import { fileURLToPath } from "url";
-// import { dirname, join } from "path";
-import path, { dirname } from "path";
+import { dirname, join } from "path";
 import uniqid from "uniqid";
-import { send } from "process";
-
-// 1) start from the current file
-
-const authorsFile = fileURLToPath(import.meta.url);
-
-// 2) get the current folder the file current file is in
-const currentFolderPath = dirname(authorsFile);
-
-// 3) Concatenate  current folder path with blog.json
-const authorsFilePath = path.join(currentFolderPath, "authors.json");
 
 const authorsRouter = express.Router();
-console.log(authorsRouter);
 
-console.log("IMPORT META URL: ", import.meta.url);
-console.log("Current file path is:", authorsFilePath);
+// 1) start from the current file
+const authorsFilePath = fileURLToPath(import.meta.url);
 
-//---- Linking the router to each request method
+// 2) get the current folder the file current file is in
+const currentFolderPath = dirname(authorsFilePath);
+
+// 3) Concatenate  current folder path with blog.json
+const authorsFilePathJSon = join(currentFolderPath, "authors.json")
+
+
+
+// console.log("IMPORT META URL: ", import.meta.url);
+// console.log("Current file path is:", authorsFilePath);
+
 
 //Fetch Authors
-authorsRouter.get("/", async (req, res) => {
-  try {
-    // 1) read the content authors.json file
+authorsRouter.get("/authors", (req, res) => {
 
-    // returns buffer object only machine readable
-    const fileContent = fs.readFileSync(authorsJSONPath);
-    console.log("File Content:", JSON.parse(fileContent));
 
-    //Convert buffer contents to strings
-    const fileContentAsStrings = fileContent.toString();
+    res.send("Hello")
+    
+    const fileContent = fs.readFileSync(authorsFilePathJSon);
+    // console.log("File Content:", JSON.parse(fileContent));
+    // const fileContentArray = fileContent.toString();
 
-    // Parsing the files of strings to a json readable file
-    const authorsArray = JSON.parse(fileContentAsStrings);
-
-    // Sends back the response
+    const authorsArray = (fileContent);
     res.send(authorsArray);
-  } catch (error) {
-    res.sen(500).send({ mesage: error.message });
-  }
+
 });
 
 //Post a new author
-authorsRouter.post("/", async (req, res) => {
-  // console.log("The res is:", res);
-
-  try {
-    // 1) read the request body
-    const { name, surname, email, dateOfBirth } = req.body;
-    // console.log("BODY: ", req.body);
-
-    // 2)add some server generated  info(id, creation date)
-    const author = {
-      id: uniqid(),
-      name,
-      surname,
-      email,
-      dateOfBirth,
-      avatar: ` https://ui-avatars.com/api/?name=${name}+${surname}`,
-      createdAt: new Date(),
-      updatedAt: new Date(),
-    };
-
-    const fileAsABuffer = fs.readFileSync(authorsFilePath);
-    const fileAsASttring = fileAsABuffer.toString();
-    const fileAsJson = JSON.parse(fileAsASttring);
-
-    console.log(author);
-
-    // 4) add new authors to the array(push)
-    // authors.push(newauthors);
-    fileAsJson.push(author);
-
-    // 5) write the array back to the file
-    // fs.writeFileSync(authorsJSONPath, JSON.stringify(authors));
-    fs.writeFileSync(authorsFilePath.JSON.stringify(fileAsJson));
-
-    // const author = { ...req.body, createdAt: new Date(), id: uniqid() };
-    // console.log(author);
-
-    // 3) read authors.json obtaining an array
-    // const authors = JSON.parse(fs.readFileSync(authorsJSONPath));
-
-    // 6) Send back appropriate response
-    res.status(201).send({ id: author.id });
-  } catch (error) {
-    res.sen(500).send({ mesage: error.message });
-  }
+authorsRouter.post("/authors", (req, res) => {
+  const newAuthor = { ...req.body, created: new Date(), id: uniqid()}
+  const authors = JSON.parse(fs.readFileSync(authorsFilePathJSon))
+  authors.push(newAuthor)
+  fs.writeFileSync(authorsFilePathJSon, JSON.stringify(authors))
+  res.status(201).send({id: newAuthor.id})
 });
 
 //delete an author (by Id)
-authorsRouter.delete("/:id", async (req, res) => {
+authorsRouter.delete("/authors/:id", async (req, res) => {
   try {
   } catch (error) {
-    res.sen(500).send({ mesage: error.message });
+    res.send(500).send({ mesage: error.message });
   }
 });
 //edit an author (id)
 authorsRouter.put("/:id", async (req, res) => {
   try {
   } catch (error) {
-    res.sen(500).send({ mesage: error.message });
+    res.status(500).send({ mesage: error.message });
   }
 });
 
 
 //Fetch an author by Id
-authorsRouter.get("/:id", async (req, res) => {
+authorsRouter.get("/authors/:id", async (req, res) => {
   try {
     const fileAsABuffer = fs.readFileSync(authorsFilePath);
     const fileAsASttring = fileAsABuffer.toString();
@@ -125,7 +76,7 @@ authorsRouter.get("/:id", async (req, res) => {
     }
 
   } catch (error) {
-    res.sen(500).send({ mesage: error.message });
+    res.status(500).send({ mesage: error.message });
   }
 });
 
@@ -144,7 +95,7 @@ authorsRouter.get("/:id", async (req, res) => {
 // })
 
 // Edit blog by ID
-authorsRouter.put("/:blogId", (req, res) => {
+authorsRouter.put("/authors/Id", (req, res) => {
 
     // 1) Read blog content
     const authors = JSON.parse(fs.readFileSync(usersJSONPath))
@@ -161,7 +112,7 @@ authorsRouter.put("/:blogId", (req, res) => {
 })
 
 // Delete author
-authorsRouter.delete("/:id", async (req, res) => {
+authorsRouter.delete("authors/:id", async (req, res) => {
   try {
     const fileAsABuffer = fs.readFileSync(authorsFilePath);
     const fileAsASttring = fileAsABuffer.toString();
@@ -178,7 +129,7 @@ authorsRouter.delete("/:id", async (req, res) => {
     res.status(204).send()
 
   } catch (error) {
-    res.sen(500).send({ mesage: error.message });
+    res.send(500).send({ mesage: error.message });
   }
 });
 
--- src/server.js
+++ src/server.js
@@ -1,7 +1,4 @@
 import express from "express"
-// import express from "express"
-
-// import cors from "cors"
 
 import listEndpoints from "express-list-endpoints"
 
@@ -10,12 +7,12 @@ import authorsRouter from "./authors/index.js"
 const server = express()
 const port =  3001
 
-server.use(express)
+server.use(express.json())
 //Need to add this line before Endpoints request bodies, other wise bodies will be undefined.
 
 
-//Letting the server to access the authors/idex.js file
-server.use("/authors", authorsRouter)
+//Letting the server to access the authors/index.js file
+server.use("/", authorsRouter)
 
 console.log("The list of end Points:", listEndpoints(server))
 
