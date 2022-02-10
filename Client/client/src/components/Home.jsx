3280fea6a3f769e5b7883541a2fa021aa80e63d3
Roby Morgan <roby.c.morgan@gmail.com>
2022-02-10 16:58:40 +0100

    fixed home page.

--- Client/client/src/components/Home.jsx
+++ Client/client/src/components/Home.jsx
@@ -3,9 +3,11 @@ import { Container } from "react-bootstrap"
 export const Home = () => {
     return (
         <Container>
-            <h1>
-                Welcome Home User
-            </h1>
+            <div className="d-flex justify-content-center align-items-center" style={{ height: "500px" }}>
+                <h1>
+                    Welcome Home User
+                </h1>
+            </div>
         </Container>
     )
 }
